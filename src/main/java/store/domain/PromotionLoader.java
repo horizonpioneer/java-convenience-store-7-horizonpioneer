1bfd2f8edbcc29247a3cadb3eee9d1c42acae3f3
변우영 <likevangogh24@gmail.com>
2024-11-07 19:27:33 +0900

    feat(PromotionLoader): promotions.md에서 진행중인 프로모션 정보를 불러오는 기능 추가
    - 프로모션 시작일과 종료일은 따로 가져오지 않고, 프로그램 시작 날짜가 프로모션에 포함되는지 여부를 boolean 형식으로 반환해서 저장하도록 했다.

---
+++ src/main/java/store/domain/PromotionLoader.java
@@ -0,0 +1,53 @@
+package store.domain;
+
+import store.util.Converter;
+import store.util.DateUtil;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PromotionLoader {
+
+    private static final String COMMA = ",";
+
+    public static List<Promotion> loadPromotions(String filePath) {
+        List<Promotion> promotions = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            reader.readLine();
+            readPromotions(reader, promotions);
+        } catch (IOException e) {
+            throw new UncheckedIOException("파일로부터 상품 정보를 불러오는데 실패했습니다.", e);
+        }
+        return promotions;
+    }
+
+    private static void readPromotions(BufferedReader reader, List<Promotion> promotions) throws IOException {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            promotions.add(getPromotion(line));
+        }
+    }
+
+    private static Promotion getPromotion(String line) {
+        String[] values = line.split(COMMA);
+        String name = values[0];
+        int buyCondition = Converter.convertStringToInt(values[1]);
+        int getCondition = Converter.convertStringToInt(values[2]);
+        boolean isTodayInRange = validateTodayInRange(values);
+
+        return new Promotion(name, buyCondition, getCondition, isTodayInRange);
+    }
+
+    private static boolean validateTodayInRange(String[] values) {
+        LocalDate startDate = Converter.convertStringToLocalDate(values[3]);
+        LocalDate endDate = Converter.convertStringToLocalDate(values[4]);
+
+        return DateUtil.isTodayInRange(startDate, endDate);
+    }
+}
