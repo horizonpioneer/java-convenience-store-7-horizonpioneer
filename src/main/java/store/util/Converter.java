460a565429b41d458b018a45fbb7a8a627b29e30
변우영 <likevangogh24@gmail.com>
2024-11-07 19:00:57 +0900

    feat(Converter): String 타입을 LocalDate 타입으로 변경하는 기능 추가

--- src/main/java/store/util/Converter.java
+++ src/main/java/store/util/Converter.java
@@ -1,5 +1,9 @@
 package store.util;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Converter {
 
     private Converter() {
@@ -12,5 +16,11 @@ public class Converter {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("입력값은 숫자여야 합니다.");
         }
+    }
+
+    public static LocalDate convertStringToLocalDate(String input) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY-MM-dd");
+        formatter = formatter.withLocale(Locale.KOREA);
+        return LocalDate.parse(input, formatter);
     }
 }
