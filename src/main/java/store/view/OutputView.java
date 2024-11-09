6b7b4a86f14f94fb6db295b3be6f5b27a325a3ae
변우영 <likevangogh24@gmail.com>
2024-11-09 17:55:51 +0900

    refactor(OutputView): 상품 재고를 관리하는 방식을 변경함에 따라, 출력 형식 변경
    - 금액에 세 자리마다 쉼표를 찍을 때, DecimalFormat 대신 NumberFormat 사용
    - 프로모션 진행 중인 상품의 경우, 프로모션 재고와 일반 재고 상황을 각각 출력한다. 이 때, 재고가 0이 되면 "재고 없음"을 출력한다.

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -2,31 +2,82 @@ package store.view;
 
 import store.domain.Product;
 
-import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class OutputView {
 
     private static final String SPACE = " ";
-    private static final DecimalFormat PRICE_FORMAT = new DecimalFormat("#,###");
 
     public static void printWelcomeMessage() {
         System.out.println("안녕하세요. W편의점입니다.");
     }
 
-    public static void printProducts(List<Product> products) {
+    public static void printProductInfo(List<Product> products) {
         System.out.println("현재 보유하고 있는 상품입니다.\n");
         for (Product product : products) {
-            String formattedPrice = PRICE_FORMAT.format(product.getPrice());
-            String promotionText = "";
-            if (!product.getPromotion().equals("null")) {
-                promotionText = SPACE + product.getPromotion();
-            }
-            System.out.println("- " + product.getName() + SPACE + formattedPrice + "원 " + product.getQuantity() + "개" + promotionText);
+            System.out.print(formatProductInfo(product));
         }
     }
 
     public static void requestProductAndQuantity() {
         System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+    }
+
+    public static void askForMembershipDiscount() {
+        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+    }
+
+    private static String formatProductInfo(Product product) {
+        return formatPromotionStockInfo(product) + formatRegularStockInfo(product);
+    }
+
+    private static String formatPromotionStockInfo(Product product) {
+        if (product.hasPromotionStock()) {
+            return formatPromotionAvailable(product);
+        }
+
+        if (hasPromotionButNoStock(product)) {
+            return formatPromotionUnavailable(product);
+        }
+
+        return "";
+    }
+
+    private static boolean hasPromotionButNoStock(Product product) {
+        return !product.hasPromotionStock() && !product.getPromotionName().equals("null");
+    }
+
+    private static String formatPromotionUnavailable(Product product) {
+        return String.format("- %s %s원 재고 없음 %s%n",
+                product.getName(),
+                formatPrice(product.getPrice()),
+                product.getPromotionName());
+    }
+
+    private static String formatPromotionAvailable(Product product) {
+        return String.format("- %s %s원 %d개 %s%n",
+                product.getName(),
+                formatPrice(product.getPrice()),
+                product.getPromotionStock(),
+                product.getPromotionName());
+    }
+
+    private static String formatRegularStockInfo(Product product) {
+        if (product.hasStock()) {
+            return String.format("- %s %s원 %d개%n",
+                    product.getName(),
+                    formatPrice(product.getPrice()),
+                    product.getStock());
+        }
+        return String.format("- %s %s원 재고 없음%n",
+                product.getName(),
+                formatPrice(product.getPrice()));
+    }
+
+    private static String formatPrice(int price) {
+        NumberFormat formatter = NumberFormat.getNumberInstance(Locale.KOREA);
+        return formatter.format(price);
     }
 }
