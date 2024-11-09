cf11bfe56cc3bf81e1a8b3ab1b7761ef0ea86227
변우영 <likevangogh24@gmail.com>
2024-11-09 17:47:49 +0900

    refactor(ProductLoader): product.md에서 상품 정보를 가져오는 방식 변경
    - 기존에는 같은 상품이어도 일반 재고와 프로모션 재고를 각각 다른 Product 객체로 생성하도록 했지만, 상품 객체 내에서 stock과 promotionStock으로 구분하는 방식으로 변경했다.

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -4,13 +4,15 @@ public class Product {
 
     private final String name;
     private final int price;
-    private int quantity;
+    private int stock;
+    private int promotionStock;
     private final String promotionName;
 
-    public Product(String name, int price, int quantity, String promotionName) {
+    public Product(String name, int price, String promotionName) {
         this.name = name;
         this.price = price;
-        this.quantity = quantity;
+        this.stock = 0;
+        this.promotionStock = 0;
         this.promotionName = promotionName;
     }
 
@@ -22,26 +24,61 @@ public class Product {
         return price;
     }
 
-    public int getQuantity() {
-        return quantity;
+    public int getStock() {
+        return stock;
+    }
+
+    public int getPromotionStock() {
+        return promotionStock;
     }
 
     public String getPromotionName() {
         return promotionName;
     }
 
-    public void decreaseStock(int amount) {
-        if (!hasStock(amount)) {
-            throw new IllegalArgumentException("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+    public void addStock(int quantity, boolean isPromotion) {
+        if (isPromotion) {
+            promotionStock += quantity;
+        } else {
+            stock += quantity;
         }
-        quantity -= amount;
     }
 
-    public boolean isEqualPromotion(Promotion promotion) {
-        return promotionName.equals(promotion.getName());
+    public boolean hasStock() {
+        return stock > 0;
     }
 
-    private boolean hasStock(int purchaseQuantity) {
-        return quantity > purchaseQuantity;
+    public boolean hasPromotionStock() {
+        return promotionStock > 0;
+    }
+
+    public boolean isEqualName(String name) {
+        return this.name.equals(name);
+    }
+
+    public boolean isEqualPromotionName(String name) {
+        return this.promotionName.equals(name);
+    }
+
+    public void decreaseStock(int amount) {
+        if (promotionStock + stock < amount) {
+            throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+        }
+        amount = decreasePromotionStock(amount);
+        decreaseRegularStock(amount);
+    }
+
+    private int decreasePromotionStock(int amount) {
+        if (promotionStock >= amount) {
+            promotionStock -= amount;
+            return 0;
+        }
+        int remainingAmount = amount - promotionStock;
+        promotionStock = 0;
+        return remainingAmount;
+    }
+
+    private void decreaseRegularStock(int amount) {
+        stock -= amount;
     }
 }
--- src/main/java/store/domain/ProductLoader.java
+++ src/main/java/store/domain/ProductLoader.java
@@ -7,44 +7,40 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProductLoader {
-
     private static final String COMMA = ",";
 
-    public static List<Product> readProducts(String filePath) {
-        List<Product> products = new ArrayList<>();
+    public static List<Product> loadProducts(String filePath, List<Promotion> promotions) {
+        Map<String, Product> productMap = new LinkedHashMap<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            reader.readLine(); // 첫 줄 건너뜀
-            readProducts(reader, products);
+            reader.readLine();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                processLine(line, productMap, promotions);
+            }
         } catch (IOException e) {
-            throw new UncheckedIOException("파일로부터 상품 정보를 불러오는데 실패했습니다.", e);
+            throw new UncheckedIOException("상품 목록으로부터 상품 정보를 읽어올 수 없습니다.", e);
         }
-        return products;
+        return new ArrayList<>(productMap.values());
     }
 
-    private static void readProducts(BufferedReader reader, List<Product> products) throws IOException {
-        String line;
-        while ((line = reader.readLine()) != null) {
-            products.add(getProduct(line));
-        }
-    }
-
-    private static Product getProduct(String line) {
+    private static void processLine(String line, Map<String, Product> productMap, List<Promotion> promotions) {
         String[] values = line.split(COMMA);
         String name = values[0];
         int price = Converter.convertStringToInt(values[1]);
         int quantity = Converter.convertStringToInt(values[2]);
-        String promotion = parsePromotion(values);
+        String promotionName = values[3];
 
-        return new Product(name, price, quantity, promotion);
+        Product product = productMap.computeIfAbsent(name, k -> new Product(name, price, promotionName));
+        addStock(promotionName, product, quantity);
     }
 
-    private static String parsePromotion(String[] values) {
-        if (values.length > 3 && !values[3].equals("null")) {
-            return values[3];
-        }
-        return "null";
+    private static void addStock(String promotionName, Product product, int quantity) {
+        boolean isPromotionStock = !promotionName.equals("null");
+        product.addStock(quantity, isPromotionStock);
     }
 }
