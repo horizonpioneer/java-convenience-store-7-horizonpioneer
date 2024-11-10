c2edb50a80fb5776c94374fe1cf6fff979023ffe
변우영 <likevangogh24@gmail.com>
2024-11-11 08:49:04 +0900

    refactor(Promotion): 프로모션 범위 검증 기능을 Promotion 내부로 통합

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -1,15 +1,26 @@
 package store.domain;
 
+import camp.nextstep.edu.missionutils.DateTimes;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Promotion {
 
     private final String name;
     private final int buyCondition;
     private final int getCondition;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
 
-    public Promotion(String name, int buyCondition, int getCondition) {
+
+    public Promotion(String name, int buyCondition, int getCondition,
+                     LocalDate startDate, LocalDate endDate) {
         this.name = name;
         this.buyCondition = buyCondition;
         this.getCondition = getCondition;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
 
     public String getName() {
@@ -26,5 +37,18 @@ public class Promotion {
 
     public int calculateAdditionalBonus(int purchaseQuantity) {
         return (buyCondition + getCondition) * (purchaseQuantity / buyCondition) - purchaseQuantity;
+    }
+
+    public boolean validateTodayInRange() {
+
+        return isTodayInRange(startDate, endDate);
+    }
+
+    private boolean isTodayInRange(LocalDate startDate, LocalDate endDate) {
+        LocalDateTime todayDateTime = DateTimes.now();
+        LocalDate today = todayDateTime.toLocalDate();
+
+        return (today.isEqual(startDate) || today.isAfter(startDate)) &&
+                (today.isEqual(endDate) || today.isBefore(endDate));
     }
 }
