099f075dd5b70bd33c8454c70d22797e9c2fbf29
변우영 <likevangogh24@gmail.com>
2024-11-11 20:02:17 +0900

    feat(InputView): 입력값이 비어있거나 공백 문자를 포함하는지 검증하는 기능 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,26 +1,39 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import store.enums.ErrorMessage;
 
 public class InputView {
 
     public static String inputProductNameAndQuantity() {
-        return Console.readLine();
+        return readLine();
     }
 
     public static String inputWhetherToAddBonusQuantity() {
-        return Console.readLine();
+        return readLine();
     }
 
     public static String inputWhetherToPayFullPriceForSome() {
-        return Console.readLine();
+        return readLine();
     }
 
     public static String inputWhetherToApplyMembership() {
-        return Console.readLine();
+        return readLine();
     }
 
     public static String inputWhetherToContinueShopping() {
-        return Console.readLine();
+        return readLine();
+    }
+
+    private static String readLine() {
+        String input = Console.readLine();
+        validateNonBlank(input);
+        return input;
+    }
+
+    private static void validateNonBlank(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.INCORRECT_INPUT.getMessage());
+        }
     }
 }
