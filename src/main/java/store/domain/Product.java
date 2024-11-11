fcb730e984cd0db896e7ade0ba5797610349cb83
변우영 <likevangogh24@gmail.com>
2024-11-11 18:48:57 +0900

    type(Product): 예외메시지를 enum 타입으로 변경

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,5 +1,7 @@
 package store.domain;
 
+import store.enums.ErrorMessage;
+
 public class Product {
 
     private final String name;
@@ -66,7 +68,7 @@ public class Product {
 
     public void decreaseStock(int amount) {
         if (promotionStock + stock < amount) {
-            throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(ErrorMessage.STOCK_EXCEEDED.getMessage());
         }
         amount = decreasePromotionStock(amount);
         decreaseRegularStock(amount);
