22b76ec1010f78cdbfcfa4e5d071de94dab2c5f5
변우영 <likevangogh24@gmail.com>
2024-11-11 20:33:46 +0900

    type(ErrorMessage): enum의 상수 이름을 의미에 맞게 수정

--- src/main/java/store/enums/ErrorMessage.java
+++ src/main/java/store/enums/ErrorMessage.java
@@ -2,7 +2,7 @@ package store.enums;
 
 public enum ErrorMessage {
     INCORRECT_INPUT("잘못된 입력입니다. 다시 입력해 주세요."),
-    PURCHASE_FORMAT("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
+    INVALID_FORMAT("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
     NON_EXISTENT_PRODUCT("존재하지 않는 상품입니다. 다시 입력해 주세요."),
     STOCK_EXCEEDED("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
 
